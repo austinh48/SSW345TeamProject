efb8dad03c997e8a342e0fccf901e8a1f58c0489
austinhodges48 <austin.hodges48@yahoo.com>
2022-04-21 19:21:48 -0400

    basic assignment upload works

--- remindBot.py
+++ remindBot.py
@@ -3,8 +3,6 @@ from discord.ext import commands
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
-from firebase_admin import db
-
 
 from config import *
 
@@ -28,13 +26,10 @@ async def on_ready():
     print(f'Logged in as {bot.user} (ID: {bot.user.id})')
     print('------')
 
-
 #add assignment command
-@bot.command()
-async def add(assignment, Name, dueDate, priority):
-    await assignment.send('Assignment added')
-
-
+#@bot.command()
+#async def add(assignment, Name, dueDate, priority):
+#    await assignment.send('Assignment added')
 
 #test firebase write
 @bot.command()
@@ -52,6 +47,22 @@ async def readTest(ctx):
     
     for doc in docs:
         print(f'{doc.id} => {doc.to_dict()}')
+
+#create an assignment
+@bot.command()
+async def add(ctx, assignmentName, dueDate, priority):
+    #make a new doc for each user
+    doc_ref = dBase.collection(u'Assignments').document()
+    doc_ref.set({
+        u'Assignment Name': assignmentName,
+        u'Due Date': dueDate,
+        u'Priority': priority
+    })
+
+
+
+
+
 
 
 bot.run(token)
