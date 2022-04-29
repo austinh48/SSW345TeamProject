28da628baa4fef4ffee1474d831e0d2cda0bc82a
austinhodges48 <austin.hodges48@yahoo.com>
2022-04-28 22:17:05 -0400

    updated bot

--- config.py
+++ config.py
@@ -1,19 +1,19 @@
 from stat import FILE_ATTRIBUTE_SPARSE_FILE
 
 
-token = 'token'
+token = 'x'
 
 firebase_config = {
-  "type": "service_account",
-  "project_id": "remindbot-9096f",
-  "private_key_id": "17ae2d81ff007d0cc2b47f06bfb1facd61881473",
-  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQC4RuwnLspmT0H3\nM8DDVSu+W4/k+cq0cK27IYrM0AWOAEOyh8542RhgR3Ucssy9Qm9sOHmKHlCIWQPI\nr9PTuMIp5ayf9ykz43hNnVWAOpJEV9l3QpRXGJUdPXLy91EsXHl2nL0xZ/0E0NFy\nWMv7Rd7Rotc/e/HSBdQjQz54a3x2j9ZIuz51SmOn8J0cwD5K/to1vulBSpPtW7xu\n4CG6PdPLO/sr93B27Gh16mc8UxOjx1ig9Ghoh44KvnNzW7Tb9Z3l+F7VwEMiSyX9\njh62qB7+QTf8+rWvhzOfm4smbU+Xo1LnmDt8J3aFErnh+JprzqLO77khpr7yN5gM\nI76H3FlbAgMBAAECggEAO4l+hvJRJlj5IRsxbmq4JVqD1SD3YvD80Hb311BmWwiu\n1qGWDelmPMUZi1ohWAx832QnsH422raXyGE2eTM6WDsCWPSL9V3K4piwpStHUpln\ng2aZcsJYewf9eY56RXDjNXVSkwHb95BX+y/BR7+9S9Hm5XXwHUEQ1wvExTdFETC/\nIG8TPb0Jhwi70fDos+jRifwf5DMvWQ2GA7fae0XIvZ1QRCVF8bKQExIxqGxzurkK\nAOakeP4iKdq3hxoolVjFQf3Jt6AdFHcBSW5Wdg/bwb9fUli1No9IeimZggiTEhJD\nw7y1+Z3Cp7Jagbu832jXb0/7JTlDhHXf1Oy8S9A7sQKBgQDmBtGrWtepCATjnEix\nciOroa0LIpTdQJPBWnweXMS/xy9O4e2+s5jrX8Az0EobufKYQGov91NLiiMwt1rO\nf5nQADgpvmN/Dtl2jqh9iMG37ZdUasEEn+1f0C8Tj0Lt9MTlkOe9KiQMKe9wePJw\n4H7EVnicqDPfdG/7F3MDvvrN5wKBgQDNFajSqg2puLoYf84oRSSHQ1vo/091k+/q\nCp+RSqhBHrIkbMp8NyUjHXChULvaySdeJ/90odLxbMq8Eyd4wY/uK1qnA133Z4Td\nDoSFiTnssRzIQCHCF8rH0sir+FishAW/Km7lG67Hsx5siyoDyM6ETJvWBZICN871\n4eUpnzYibQKBgQC1SBB7jAdERnab7rH3bm2gOLWIfgN17wvDKQ2CXDgOr+qfrzxP\nu3i7nuV5sCCHg18mEDXfgXpaoQ4kuYvODA/rNhENDOea3dgSMFGnRVzuGEfYFp3x\niBxqE0bWkmP75pg6F54anZh1IaN7L0ZaIQKmt0Msqv7ejwQljZBuFlfQZwKBgFgL\nvHb02vjKfdqWxAM17ADGQmc9dPedk5p/BRJzYtjSPNi2fBfn9OGmefxHlN7RDT23\nHk0MTGupsUAdWQzWzNwLcNfS8jyNDNV6rhpGV7/uKdUu809It54iOFTcQfWOmIkB\nxqpFwDusApvHAzqsszzJRv4TViPtocSePqwn1i3hAoGBAISgykJFQNJ3METG+rN5\n2nYe61GyHdWRCt9PvNhCa8IJL+WiYrS6BsZhImcTIySamlFIP0zmC+w6CdIJpYnv\nIG0xF1Ps6gixOtb+k+ji0bUb1sLSOStSh23hmhCDw3KEzGWR0SYp6LfJ/pD1dF7Z\nkMNI6OyBGwfVdcNvk+SuSwNa\n-----END PRIVATE KEY-----\n",
-  "client_email": "firebase-adminsdk-c87ex@remindbot-9096f.iam.gserviceaccount.com",
-  "client_id": "111816754052773306955",
-  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "token_uri": "https://oauth2.googleapis.com/token",
-  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-c87ex%40remindbot-9096f.iam.gserviceaccount.com"
+  "type": "x,
+  "project_id": "x,
+  "private_key_id": "x,
+  "private_key": "x",
+  "client_email": "x",
+  "client_id": "x",
+  "auth_uri": "x",
+  "token_uri": "x",
+  "auth_provider_x509_cert_url": "x",
+  "client_x509_cert_url": "x"
 }
 
-databaseUrl = "https://remindbot-9096f-default-rtdb.firebaseio.com/"
+databaseUrl = "x"
